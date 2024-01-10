8592142242235e8f0a25d9cb2f039f13e5c63d61
Pichala Revanth Kumar Reddy <pichala.revanthkumarreddy@lumen.com>
2024-01-10 14:25:27 +0530

    response changed

--- src/main/java/com/example/demo/apis/SampleApi.java
+++ src/main/java/com/example/demo/apis/SampleApi.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class SampleApi {
     @GetMapping
     public ResponseEntity<String> get(){
-        return ResponseEntity.ok("Hello, sample");
+        return ResponseEntity.ok("Hello, master");
     }
 }
